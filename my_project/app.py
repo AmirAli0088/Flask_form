4d80b653053fed215f6ef4928433538a4893d74c
AmirAli0088 <amirali.abd0088@gmail.com>
2025-02-15 20:46:55 +0330

    Add files via upload

---
+++ my_project/app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request
+import sqlite3
+import smtplib
+from email.mime.text import MIMEText
+import os
+
+app = Flask(__name__)
+
+DB_PATH = "/tmp/database.db"
+
+
+def init_db():
+    # conn = sqlite3.connect(DB_PATH)
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS users (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        first_name TEXT NOT NULL,
+                        last_name TEXT NOT NULL)
+                   ''')
+    conn.commit()
+    conn.close()
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+
+        conn = sqlite3.connect(DB_PATH)
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO users (first_name, last_name) VALUES (?, ?)", (first_name, last_name))
+        conn.commit()
+        conn.close()
+
+        send_email(first_name, last_name)
+
+        return "اطلاعات شما ثبت شد و ارسال گردید!"
+
+    return render_template('index.html')
+
+
+def send_email(first_name, last_name):
+    sender_email = os.getenv("EMAIL_USER")
+    receiver_email = "amirali.abd.0088@gmail.com"
+    password = os.getenv("EMAIL_PASS")
+
+    message = MIMEText(f"name: {first_name}\nfamily: {last_name}")
+    message['Subject'] = 'اطلاعات جدید کاربر'
+    message['From'] = sender_email
+    message['To'] = receiver_email
+
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+        server.quit()
+    except Exception as e:
+        print("خطا در ارسال ایمیل:", e)
+
+
+if __name__ == '__main__':
+    init_db()
+    app.run(debug=True)
