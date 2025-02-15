8da9cdfe22609253b41dfcad16af84d99cbf4314
AmirAli0088 <amirali.abd0088@gmail.com>
2025-02-15 22:20:22 +0330

    Update app.py

--- my_project/app.py
+++ my_project/app.py
@@ -8,28 +8,33 @@ app = Flask(__name__)
 # لیست موقت برای ذخیره کاربران (به جای دیتابیس)
 users = []
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         first_name = request.form['first_name']
         last_name = request.form['last_name']
+        phone_number = request.form['last_name']
+        email_address = request.form['last_name']
 
         # ذخیره در لیست به جای دیتابیس
-        users.append({"first_name": first_name, "last_name": last_name})
+        users.append({"first_name": first_name, "last_name": last_name, "email_address": email_address,
+                      "phone_number": phone_number})
 
         # ارسال ایمیل
-        send_email(first_name, last_name)
+        send_email(first_name, last_name, email_address, phone_number)
 
         return "اطلاعات شما ثبت شد و ارسال گردید!"
 
     return render_template('index.html', users=users)
 
-def send_email(first_name, last_name):
+
+def send_email(first_name, last_name, email_address, phone_number):
     sender_email = os.getenv("amirali.abd.0088@gmail.com")
     receiver_email = "amirali.abd.0088@gmail.com"
     password = os.getenv("hutw xqvc vuwk atk")
 
-    message = MIMEText(f"name: {first_name}\nfamily: {last_name}")
+    message = MIMEText(f"name: {first_name}\nfamily: {last_name}\nemail: {email_address}\nphone: {phone_number}")
     message['Subject'] = 'اطلاعات جدید کاربر'
     message['From'] = sender_email
     message['To'] = receiver_email
@@ -42,6 +47,7 @@ def send_email(first_name, last_name):
         server.quit()
     except Exception as e:
         print("خطا در ارسال ایمیل:", e)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
